0c10a299a0137cb6a2fe8a0fd45c467e66f2ce8b
juanfilhippisantos-ui <juan.filhippi.santos@escola.pr.gov.br>
2025-10-22 08:04:06 -0300

    jogo

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 2;
-console.log(numeroSecreto)
-let chute = prompt('Escolha um número entre 1 e 10');
+// código omitido
 
-// se chute for igual ao número secreto
-if (chute == numeroSecreto) {
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+alert('Boas vindas ao jogo do número secreto');
+let numeroSecreto = 5;
+console.log(numeroSecreto)
+let chute
+
+// enquanto chute não for igual ao n.s.
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10');
+    // se chute for igual ao número secreto
+if (chute == numeroSecreto) { 
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
 } else {
-    alert('Você errou :(' )
+    if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+} else {
+        alert(`0 número secreto é maior que ${chute}`)
 }
+
+// código omitido
